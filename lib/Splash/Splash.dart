5ac6be2ceb1575359c030de51d6a3f4d2cdf7f2b
Gabriel Menoi <ga.gabrielmenoi@gmail.com>
2022-06-03 20:49:15 -0300

    first commit

---
+++ lib/Splash/Splash.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:projects/home/home.dart';
+
+class Splash extends StatefulWidget {
+
+  const Splash({Key? key}) : super(key: key);
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+
+  @override
+  void initState() {
+    super.initState();
+        Timer(Duration(seconds: 5), () {
+          Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => Home()));
+        });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // Padding(
+          //   padding:
+          //   EdgeInsets.only(left: 20, right: 20, top: 44.92, bottom: 46.55),
+          //   child: Image.asset(
+          //     "assets/images/image.jpg",
+          //     width: 559.67,
+          //     height: 109.14,
+          //     alignment: Alignment.center,
+          //   ),
+          // ),
+        ],
+      ),
+    );
+  }
+
+
+}
---
+++ lib/app.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:projects/Splash/Splash.dart';
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+        statusBarColor: Color.fromRGBO(57, 49, 153, 1),
+        statusBarBrightness: Brightness.light,
+        statusBarIconBrightness: Brightness.light
+    ));
+
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // theme: DefaultTheme.themeData,
+      home: Splash(),
+    );
+  }
+}
